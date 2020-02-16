0ee46ae8a10979482aca8d105674dd16b2696759
Mahmoud Ali <mahmoudali5@outlook.com>
2020-02-16 21:28:27 +0200

    init commit

---
+++ app/Models/Service.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Service extends Authenticatable
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+}
---
+++ app/Models/Vehicle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Vehicle extends Authenticatable
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+}
